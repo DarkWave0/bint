76d49e4f88c53dec8cfee3d251a39e29d118d129
Tom Willemsen <tom@picturefix.nl>
2017-01-14 17:52:27 +0100

    Add directory selection dialog
    Stop using Tkinter directly and use Tix instead so we have access to
    the DirectoryDialog widget.

--- bint/ui.py
+++ bint/ui.py
@@ -1,24 +1,33 @@
 import tkinter as tk
+import tkinter.ttk as ttk
+import tkinter.tix as tix
 
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.pack()
-        self.create_widgets()
+        self.create_widgets(master)
 
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
+    def create_widgets(self, master):
+        top_frame = ttk.Frame(self)
+        top_frame.pack(side="top")
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=root.destroy)
+        btn = ttk.Button(self)
+        btn['text'] = 'Browse'
+        btn['command'] = self.select_dir
+        btn.pack()
+
+        self.quit = ttk.Button(self, text="QUIT", command=root.destroy)
         self.quit.pack(side="bottom")
 
-    def say_hi(self):
-        print("hi there, everyone!")
+    def select_dir(self):
+        dir_entry = tix.DirSelectDialog(self)
+        dir_entry['command'] = self.dir_selected
+        dir_entry.popup()
 
-root = tk.Tk()
+    def dir_selected(self, directory):
+        print('Directory: ', directory)
+
+root = tix.Tk()
 app = Application(master=root)
 app.mainloop()
